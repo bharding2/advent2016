1645c3bbdbd61c3b2b566bf98cd2f60d80656c2e
bharding2 <b.harding@gmail.com>
2017-12-03 13:05:01 -0800

    commenting task 2

--- 2017/day3/task2.js
+++ 2017/day3/task2.js
@@ -1,5 +1,11 @@
+// so it turns out we have to build that shitty grid anyways
+// this is a mess, but basically we are going to build one level
+// at a time until we have the one that has a cell with a higher
+// value than the target value
+
 var inputValue = 289326;
 
+// initializing grid with center cell
 var grid = {
   1: {
     x: 0,
@@ -8,9 +14,14 @@ var grid = {
   }
 };
 
+// our function to get the value of a cell.  It walks down the grid
+// from the provided cell number, checks all adjacent cells to see
+// if they contain a value then sums them
 function getValue(currentCell) {
   var newValue = 0;
 
+  // here we are checking all adjacent squares, aka all cells that
+  // are zero one one steps away on both the x and y axis
   for (var i = 1; i < currentCell; i++) {
     var xDiff = Math.abs(grid[currentCell].x - grid[i].x);
     var yDiff = Math.abs(grid[currentCell].y - grid[i].y);
@@ -23,7 +34,10 @@ function getValue(currentCell) {
   return newValue;
 };
 
+// we are creating a new outer level with a certain number of
+// sides, this logic is similar to the task 1 solution
 function createLevel(sides) {
+  // initializing current cell at highest cell number on level
   var currentCell = sides ** 2
 
   var corner1 = currentCell;
@@ -31,13 +45,17 @@ function createLevel(sides) {
   var corner3 = corner2 - (sides - 1);
   var corner4 = corner3 - (sides - 1);
 
+  // lowest cell on level
   var lastCell = corner4 - (sides - 2);
 
+  // building the cells on the level
   while (currentCell >= lastCell) {
     var cellX = 0;
     var cellY = 0;
     var midPoint = 0;
 
+    // checking which side of the box it is on, then find
+    // the x and y coordinates with center cell being 0,0
     if (currentCell <= corner4) {
       midPoint = corner4 - Math.floor(sides / 2);
 
@@ -69,14 +87,23 @@ function createLevel(sides) {
   }
 }
 
+// we initialized the grid with the center cell, so we start
+// with the next level, which has 3 sides
 var sides = 3;
 var bigCell = 0;
 var bigValue = 0;
 
+// while the largest value on the outer level is less than the
+// target value, we will create a new level
 while (bigValue < inputValue) {
   bigCell = (sides) ** 2;
 
+  // this creates the cells on that level
   createLevel(sides);
+
+  // this next super ugly bit walks through all of the cells and
+  // updates the values, starting with cell 2 because we initialized
+  // the grid with cell 1
   var gridKeys = Object.keys(grid);
 
   for (var i = 2; i < gridKeys.length + 1; i++) {
@@ -87,8 +114,9 @@ while (bigValue < inputValue) {
   sides += 2;
 }
 
+// this other ugly bit is just finding the target cell on our grid
+// with a larger value than our input and then logging that value
 var allGridKeys = Object.keys(grid);
-
 var targetValue = 0;
 
 allGridKeys.forEach((gridKey) => {

faddd1daad094893647fee2fa4bd63939da79bbc
bharding2 <b.harding@gmail.com>
2017-12-09 00:58:35 -0800

    further refactors

--- 2017/day8/task2.js
+++ 2017/day8/task2.js
@@ -16,17 +16,17 @@ input.forEach((line) => {
   var condition = line.split(' if ')[1];
 
   operation = operation.replace('inc', '+=');
-  operation = operation.replace('dec', '-=')
+  operation = operation.replace('dec', '-=');
 
   var operationRegister = operation.split(' ')[0];
   var conditionRegister = condition.split(' ')[0];
 
-  operation = operation.replace(operationRegister, 'registers["' + operationRegister + '"]')
-  condition = condition.replace(conditionRegister, 'registers["' + conditionRegister + '"]')
+  operation = operation.replace(operationRegister, 'registers[operationRegister]');
+  condition = condition.replace(conditionRegister, 'registers[conditionRegister]');
 
   if (eval(condition)) eval(operation);
 
-  if (registers[operationRegister] > registerMax) registerMax = registers[operationRegister]
+  if (registers[operationRegister] > registerMax) registerMax = registers[operationRegister];
 });
 
 console.log(registerMax);
